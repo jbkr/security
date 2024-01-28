240892c0eff30f4fe727926ed88c76597016fa79
jbkr <jb_kr@yahoo.co.kr>
2024-01-28 11:24:26 +0900

    dd

--- src/main/java/hello/world/v1/WebSecurityConfig.java
+++ src/main/java/hello/world/v1/WebSecurityConfig.java
@@ -18,10 +18,10 @@ public class WebSecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
 			.authorizeHttpRequests((requests) -> requests
-				.requestMatchers("/", "/home").permitAll()
+				.requestMatchers("/").permitAll()
 				.anyRequest().authenticated()
-			)
-			.formLogin((form) -> form
+			);
+			http.formLogin((form) -> form
 				.loginPage("/login")
 				.permitAll()
 			)
