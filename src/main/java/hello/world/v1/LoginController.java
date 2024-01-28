b96b653b42504898dad29cad8e531e35d68991c7
jbkr <jb_kr@yahoo.co.kr>
2024-01-28 10:57:54 +0900

    first

---
+++ src/main/java/hello/world/v1/LoginController.java
@@ -0,0 +1,12 @@
+package hello.world.v1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+}
---
+++ src/main/java/hello/world/v1/LoginProjectV1Application.java
@@ -0,0 +1,13 @@
+package hello.world.v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginProjectV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginProjectV1Application.class, args);
+	}
+
+}
